41066c64af2b1faf7d41e1f51e22dc6973ec7de3
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-04-25 21:40:38 +0300

    triangle anim

---
+++ js/circles.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    // var scrollCircle = $.scrollorama({
+    //     blocks: '.circles'
+    // });
+
+    // scrollCircle.animate('.circle__quality', {
+    //     duration: 100, property: 'margin-top', start: -35 + '%', end: -7 + '%'
+    // });
+
+    // scrollorama.animate('#aboutScroll',{
+    //     duration:200, property:'height', start:7 +'em', end: 30 + 'em', baseline: "bottom"}
+    //     )
+
+})
