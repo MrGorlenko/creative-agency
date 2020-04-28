5dc5b9a5f4125f29854eab0f2087f0cc203d90d2
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-04-28 23:35:18 +0300

    header mobile

--- js/circles.js
+++ js/circles.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
 
-    // var scrollCircle = $.scrollorama({
-    //     blocks: '.circles'
-    // });
+    $('.reason').on('wheel', function(){
+        $('.header').css('display', 'none');
+    })
 
-    // scrollCircle.animate('.circle__quality', {
-    //     duration: 100, property: 'margin-top', start: -35 + '%', end: -7 + '%'
-    // });
-
-    // scrollorama.animate('#aboutScroll',{
-    //     duration:200, property:'height', start:7 +'em', end: 30 + 'em', baseline: "bottom"}
-    //     )
+    $('.slider').on('wheel', function(){
+        $('.header').css('display', 'flex');
+    })
 
 })
