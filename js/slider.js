65c4d7c0686d875d6c787a504f9c3f1ed0cc4d99
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-05-05 15:55:17 +0300

    menu fix

--- js/slider.js
+++ js/slider.js
@@ -48,6 +48,41 @@
       if (k.matches) { 
       
 
+        var i = 1;
+    $('#upper-menu').on('click', function(){
+
+
+        i = i + 1
+
+        if (i%2 == 0){
+            $(".global-menu").addClass('global-menu_active');
+
+            $('.line-up').addClass('global-menu_span-up');
+    
+            $('.line-down').addClass('global-menu_span-down');
+
+            setTimeout(function () {
+                $(".global-menu").addClass('global-menu_bg');
+                $(".global-menu").removeClass('global-menu_opacity-none');
+            }, 200);
+            setTimeout(function () {
+                $(".global-menu__link").addClass('global-menu_opacity');
+                $(".global-menu__link").removeClass('global-menu_opacity-none');
+            }, 200);
+            } else {
+
+            setTimeout(function () {
+                $(".global-menu").removeClass('global-menu_active');
+            }, 1200);
+
+            $('.line-up').removeClass('global-menu_span-up');
+    
+            $('.line-down').removeClass('global-menu_span-down');
+    
+            $(".global-menu").addClass('global-menu_opacity-none');
+            $(".global-menu__link").addClass('global-menu_opacity-none');
+            }
+    })
 
 
         var j = 1;
